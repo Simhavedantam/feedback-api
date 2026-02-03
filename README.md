7eb7ae38f83fc9d5e5e155d5dad267641efa89cb
Simhavedantam <simhavedantamofficial@gmail.com>
2026-02-04 03:24:16 +0530

    Inital just deployment - no clean code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # feedback-api
+This is to have a test space of GCP cloud in my space a test for feedback project
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return jsonify({
+        "status": "ok",
+        "message": "Cloud Run is working"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
